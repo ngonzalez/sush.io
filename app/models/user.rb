55262932dcd639f2230da1405130af18aba4a4e5
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-10-07 14:24:57 +0200

    Users: List starred repos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :repositories
   serialize :remote_starred_ids, Array
+  def starred_repositories
+    repositories.select do |item|
+      remote_starred_ids.include? item.remote_id
+    end
+  end
 end
