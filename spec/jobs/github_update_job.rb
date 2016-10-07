2d2750e601c65a9adbe5625a14fdcfb4530bb961
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-10-07 20:14:37 +0200

    Specs: Update GitHub Job tests, simplify stubs

--- spec/jobs/github_update_job.rb
+++ spec/jobs/github_update_job.rb
@@ -15,7 +15,7 @@ RSpec.describe GithubUpdateJob, :type => :job do
         allow_any_instance_of(Octokit::Configurable).to receive(:login).and_return(true)
         content = JSON.parse File.read(Rails.root.join("spec/web_mocks/github_user"))
         response = content.deep_symbolize_keys
-        stub_request(:get, "https://api.github.com/user").with(:headers => {'Accept'=>'application/vnd.github.v3+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>"token #{GITHUB_TOKEN}", 'Content-Type'=>'application/json', 'User-Agent'=>'Octokit Ruby Gem 4.3.0'}).to_return(status: 200, body: response.to_s)
+        stub_request(:get, "https://api.github.com/user").to_return(status: 200, body: response.to_s)
         content = JSON.parse File.read(Rails.root.join("spec/web_mocks/github_starred"))
         response = content.each_with_object([]){ |item, array| array << item.deep_symbolize_keys }
         stub_request(:get, "https://api.github.com/users/#{@user.name}/starred?page=1&per_page=#{API_PER_PAGE}").to_return(status: 200, body: response)
