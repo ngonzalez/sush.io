2ffd67534c6227bf7f9e9975e50f426c00934cf3
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-10-07 19:42:01 +0200

    Users List: Fix GitHub External Urls

--- app/helpers/github_helper.rb
+++ app/helpers/github_helper.rb
@@ -1,7 +1,7 @@
 module GithubHelper
   def user_starred_repositories user
     user.starred_repositories.each_with_object([]) do |repository, array|
-      array << link_to(repository.name, [user.name, repository.name].join('/'), target: '_blank')
+      array << link_to(repository.name, GITHUB_URL + [user.name, repository.name].join('/'), target: '_blank')
     end.join ", "
   end
 end
--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,5 @@
 WebMock.disable_net_connect! if Rails.env.test?
-GITHUB_TOKEN = Rails.env.test? ? "TEST_GITHUB_TOKEN" : "REPLACE_TOKEN"
+GITHUB_URL = "http://github.com/"
+GITHUB_TOKEN = "REPLACE_TOKEN"
 ITEMS_PER_PAGE = 5
 API_PER_PAGE = 100
