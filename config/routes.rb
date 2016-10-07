45b4a05acc67b18695b14ced001ce7a717a1979c
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2016-10-07 19:21:33 +0200

    Routes: Fix GitHub index / list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'github#index'
-  get '/github', to: 'github#list'
-  put '/github', to: 'github#update'
+  get '/github', to: 'github#index'
+  get '/github/list', to: 'github#list'
   get '/users', to: 'users#index'
 end
